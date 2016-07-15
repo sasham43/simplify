2f36ecf46785e17dd1d55294db0dc6206bf123b1
sasham43 <sashasemail@gmail.com>
2016-07-15 17:18:47 -0500

    Remove ngRouting for now, auth working

--- client/client.js
+++ client/client.js
@@ -2,24 +2,24 @@ angular.module('simplifyApp', ['ngRoute']);
 
 angular.module('simplifyApp').config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
   // fill in route info
-  $routeProvider
-    .when('/',{
-      templateUrl: '/views/splash.html',
-      controller: 'SplashController',
-      controllerAs: 'sc'
-    })
-    .when('/about', {
-      templateUrl: '/views/about.html',
-      controller: 'AboutController',
-      controllerAs: 'ac'
-    })
-    .when('/login', {
-      templateUrl: '/views/login.html',
-      controller: 'LoginController',
-      controllerAs: 'lc'
-    })
-
-    $locationProvider.html5Mode(true);
+  // $routeProvider
+  //   .when('/',{
+  //     templateUrl: '/views/splash.html',
+  //     controller: 'SplashController',
+  //     controllerAs: 'sc'
+  //   })
+  //   .when('/about', {
+  //     templateUrl: '/views/about.html',
+  //     controller: 'AboutController',
+  //     controllerAs: 'ac'
+  //   })
+  //   .when('/login', {
+  //     templateUrl: '/views/login.html',
+  //     controller: 'LoginController',
+  //     controllerAs: 'lc'
+  //   })
+  //
+  //   $locationProvider.html5Mode(true);
 }]);
 
 angular.module('simplifyApp').controller('SplashController',['$http', function($http){
@@ -27,6 +27,14 @@ angular.module('simplifyApp').controller('SplashController',['$http', function($
 }]);
 
 angular.module('simplifyApp').controller('LoginController',['$http', function($http){
+  var lc = this;
+
+  lc.spotifyAuth = function(){
+    $http.get('/authorize').then(function(response){
+      console.log('spotify auth response:', response);
+    });
+  }
+
   console.log('Login controller loaded.');
 }]);
 
--- server/server.js
+++ server/server.js
@@ -1,22 +1,27 @@
+require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
 ////////////import modules//////////
 var index = require('./routes/index');
+// var authorize = require('./routes/authorize');
 
 
 //////////// config /////////////
 app.use(express.static('server/public'));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 
 
 
 ///////////routes/////////////
 app.use('/', index);
+// app.use('/authorize', authorize);
 
 
 
