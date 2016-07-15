99835e9761ea8a6fc836683643023bc9b6a79404
sasham43 <sashasemail@gmail.com>
2016-07-15 14:09:51 -0500

    Initial commit, serving html, js, css

---
+++ client/client.js
@@ -0,0 +1,9 @@
+angular.module('simplifyApp', []);
+
+// angular.module('simplifyApp').config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+//   // fill in route info
+// }]);
+
+angular.module('simplifyApp').controller('SplashController',['$http', function($http){
+  console.log('Splash controller loaded.');
+}]);
