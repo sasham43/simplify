7b73a63fda6a02bf7b0c2b6d01ffdd0347d71e61
sasham43 <sashasemail@gmail.com>
2016-07-20 15:13:33 -0500

    Add more tables

--- server/db/connection.js
+++ server/db/connection.js
@@ -6,7 +6,8 @@ var connectionString = 'postgres://localhost:5432/simplify';
 function initialize(){
   async.series([
     createUsersTable,
-
+    createArtistsTable,
+    createAlbumsTable
   ], function(err){
     if(err){
       console.log('Error initializing tables:', err);
@@ -16,7 +17,7 @@ function initialize(){
   });
 }
 
-function createUsersTable(){
+function createUsersTable(callback){
   pg.connect(connectionString, function(err, client, done){
     if (err){
       console.log('Error connecting to database:', err);
@@ -36,19 +37,48 @@ function createUsersTable(){
       ')');
 
       query.on('end', function(){
-        console.log('Successfully created schema');
+        console.log('Successfully created user schema');
+        callback(null);
         done();
       });
 
       query.on('error', function(error){
-        console.log('Error creating schema', error);
+        console.log('Error creating user schema', error);
+        callback(error);
         process.exit(1);
       });
     }
   });
 }
 
-function createAlbumTable(){
+function createArtistsTable(callback){
+  pg.connect(connectionString, function(err, client, done){
+    if (err){
+      console.log('Error connecting to database:', err);
+      process.exit(1); // this is a built-in node thing that lets us kill this node process (i.e. our web server) if there is an error
+    } else {
+      // create inital schema
+      var query = client.query('CREATE TABLE IF NOT EXISTS artists ('+
+      'id int PRIMARY KEY,' +
+      'name varchar(80)' +
+      ')');
+
+      query.on('end', function(){
+        console.log('Successfully created artist schema.');
+        callback(null);
+        done();
+      });
+
+      query.on('error', function(error){
+        console.log('Error creating artist schema', error);
+        callback(error);
+        process.exit(1);
+      });
+    }
+  });
+}
+
+function createAlbumsTable(callback){
   pg.connect(connectionString, function(err, client, done){
     if (err){
       console.log('Error connecting to database:', err);
@@ -56,23 +86,25 @@ function createAlbumTable(){
     } else {
       // create inital schema
       var query = client.query('CREATE TABLE IF NOT EXISTS albums ('+
-      'id serial PRIMARY KEY,' +
-      'spotify_id varchar(80) NOT NULL,'+
-      'birthdate date,' +
-      'display_name varchar(80),' +
-      'email varchar(80),' +
-      'image_url varchar(100),' +
-      'product varchar(50),' +
-      'spotify_url varchar(80)' +
+      'id int PRIMARY KEY,' +
+      'artist_id int REFERENCES "artists"(id),' +
+      'image_small varchar(80),' +
+      'image_medium varchar(80),' +
+      'image_large varchar(80),' +
+      'name varchar(80),' +
+      'release_date date,' +
+      'popularity int' +
       ')');
 
       query.on('end', function(){
         console.log('Successfully created album schema.');
+        callback(null);
         done();
       });
 
       query.on('error', function(error){
-        console.log('Error creating schema', error);
+        console.log('Error creating album schema', error);
+        callback(error);
         process.exit(1);
       });
     }
